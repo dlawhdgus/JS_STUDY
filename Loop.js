051bbee800422119aef8e85e95a13f8022a76b56
dlawhdgus <212whdgus@gmil.com>
2022-09-08 10:53:31 +0900

    add another for

--- Loop.js
+++ Loop.js
@@ -4,8 +4,8 @@ let userInfo = {
     address : "sanbon"
 }
 
-for(i in userInfo){
-    console.log(`${i} : ${userInfo[i]}`)
+for(key in userInfo){
+    console.log(`${key} : ${userInfo[key]}`)
 }
 
 const arr = ['a','b','c','d'];
@@ -13,3 +13,13 @@ const arr = ['a','b','c','d'];
 arr.forEach((item, index, arr) => {
     console.log(`index : ${index} | item : ${item} | arr[${index}] : ${arr[index]}`)
 })
+
+for(let i in userInfo){
+    console.log(userInfo[i])
+}
+
+let arrA = [0,1,2,3,4,5,6,7];
+
+for(let i of arrA){
+    console.log(arrA[i])
+}
